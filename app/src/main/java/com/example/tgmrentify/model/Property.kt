0b38411a4649a51baa75b2caa415e22cdc90d075
mihilidesilva <mihililahiruka925@gmail.com>
2025-11-21 12:59:38 +0530

    commit files

---
+++ app/src/main/java/com/example/tgmrentify/model/Property.kt
@@ -0,0 +1,18 @@
+package com.example.tgmrentify.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Property(
+    val propertyId: String,
+    val landlordId: String,
+    val title: String,
+    val description: String = "A beautiful place to live with modern amenities.", // Default description
+    val location: String,
+    val rentAmount: Double,
+    val propertyType: String = "Apartment", // Default type
+    val status: String = "Available", // Default status
+    val contactNumber: String = "077-1234567", // Default contact
+    val imageUrls: List<String>
+) : Parcelable
