c76338c937d61bba5a8f2b72c627c2eb3e39cac4
Thamasha3 <nethuthamasha3@gmail.com>
2025-11-22 22:17:44 +0530

    WIP: Staging remaining local modifications before merge.

--- app/src/main/java/com/example/tgmrentify/MainActivity.kt
+++ app/src/main/java/com/example/tgmrentify/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tgmrentify
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
